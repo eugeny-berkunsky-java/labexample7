f69c4193849c69d4fe77206f4a7495d1da33b39a
kafedra-iust <kafedra.iust@gmail.com>
2020-03-27 13:02:31 +0200

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,148 @@
+package main;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    List<Person> people = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        int selection;
+        while ((selection = menu()) != 0) {
+            switch (selection) {
+                case 1:
+                    people = readFromFile();
+                    break;
+                case 2:
+                    writeToFile(people);
+                    break;
+                case 3:
+                    printList(people);
+                    break;
+                case 4:
+                    writeToBinaryFile("file.dat");
+                    break;
+                case 5:
+                    people = readFromBinaryFile("file.dat");
+                    break;
+                case 6:
+                    System.out.println("Year: ");
+                    int year = new Scanner(System.in).nextInt();
+                    List<Person> find = findByYear(people,year);
+                    printList(find);
+                    break;
+                case 7:
+                    System.out.println("id: ");
+                    int id = new Scanner(System.in).nextInt();
+                    Person p = findById(people, id);
+                    if (p!=null) deleteFromList(people, p);
+                    break;
+            }
+        }
+    }
+
+    private void deleteFromList(List<Person> people, Person p) {
+        people.remove(p);
+    }
+
+    private Person findById(List<Person> people, int id) {
+        for (Person person : people) {
+            if (person.getId() == id) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    private List<Person> findByYear(List<Person> people, int year) {
+        List<Person> list = new ArrayList<>();
+        for (Person person : people) {
+            if (person.getBirthday().isAfter(LocalDate.of(year, 12, 31))) {
+                list.add(person);
+            }
+        }
+        return list;
+    }
+
+    private List<Person> readFromBinaryFile(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (List<Person>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    private void writeToBinaryFile(String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(people);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printList(List<Person> people) {
+        System.out.println("--- People ---");
+        for (Person person : people) {
+            System.out.println(person);
+        }
+        System.out.println("--------------");
+    }
+
+    private List<Person> readFromFile() {
+        List<Person> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("people.txt"))) {
+            String line;
+            while ((line = reader.readLine())!=null) {
+                String[] s = line.split(" ");
+                int id = Integer.parseInt(s[0]);
+                String name = s[1];
+                int day = Integer.parseInt(s[2]);
+                int month = Integer.parseInt(s[3]);
+                int year = Integer.parseInt(s[4]);
+                double rating = Double.parseDouble(s[5]);
+                Person p = new Person(id, name, LocalDate.of(year, month, day), rating);
+                list.add(p);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private void writeToFile(List<Person> people) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d M yyyy");
+        try (PrintWriter out = new PrintWriter("people1.txt")) {
+            for (Person person : people) {
+                out.print(person.getId()+" "+person.getName()+" ");
+                out.print(formatter.format(person.getBirthday()));
+                out.println(" "+person.getRating());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int menu() {
+        System.out.println("1. Чтение из файла");
+        System.out.println("2. Запись в файл");
+        System.out.println("3. Вывод списка на экран");
+        System.out.println("4. Запись в бинарный файл");
+        System.out.println("5. Чтение из бинарного файла");
+        System.out.println("6. Родившиеся после заданного года");
+        System.out.println("7. Удалить по id");
+        System.out.println("--------------------------");
+        System.out.println("0. Выход");
+        return new Scanner(System.in).nextInt();
+    }
+}
---
+++ src/main/Person.java
@@ -0,0 +1,77 @@
+package main;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person implements Serializable {
+    private int id;
+    private String name;
+    private LocalDate birthday;
+    private double rating;
+
+    public Person(int id, String name, LocalDate birthday, double rating) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Double.compare(person.rating, rating) == 0 &&
+                name.equals(person.name) &&
+                Objects.equals(birthday, person.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, birthday, rating);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", rating=" + rating +
+                '}';
+    }
+}
